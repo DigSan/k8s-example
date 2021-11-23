38b9115691b1b462a44cd7b23dd32f4234f85033
Александр Поломошнов <apolomoshnov@nntc.pro>
2021-11-23 21:38:30 +0700

    deleting

--- create-client.sh
+++
@@ -1,5 +0,0 @@
-#get token
-RESULT=`curl --data "username=<your_admin_user>&password=<your_passwod>&grant_type=password&client_id=admin-cli" http://localhost:8090/auth/realms/master/protocol/openid-connect/token
-TOKEN=`echo $RESULT | sed 's/.*access_token":"//g' | sed 's/".*//g'`
-#create user
-curl -X POST -d '{ "clientId": "myclient" }' -H "Content-Type:application/json" -H "Authorization: bearer ${TOKEN}" http://localhost:8090/auth/realms/master/clients-registrations/default
