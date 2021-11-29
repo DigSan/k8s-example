e13ed18f43c62c27c2a498e0309231df154e7678
Александр Поломошнов <apolomoshnov@nntc.pro>
2021-11-29 20:17:49 +0700

    fix scripts

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,13 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml
+
+helm install auth auth/
+
 kubectl apply -f nntc-regestry.yaml
 
 helm install rabbitmq rabbitmq/
+
 helm install redis redis/
 
 helm install capital-resources capital-resources/
+
+helm install infrastructure infrastructure/
